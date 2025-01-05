2ff727afb80385ca809d23c57a60c963fad8fde1
Carmelo85S <carmelo.salis85@gmail.com>
2025-01-05 15:55:26 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Hero from "./components/hero/Hero"
+function App() {
+
+  return (
+    <>
+     <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+      <h1>HALLO</h1>
+        <section className="hero-container">
+            <form action="">
+                <input type="text"/>
+                <button type="submit "className="searchBtn">Search</button>
+            </form>
+        </section>
+    </section>
+  )
+}
+
+export default Hero
